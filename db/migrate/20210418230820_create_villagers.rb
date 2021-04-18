2763d2fda44b8e114dc738bacf7f93554984b8bd
Bhavani <bhavanivamshee@gmail.com>
2021-04-18 18:19:01 -0500

    adds table information

--- db/migrate/20210418230820_create_villagers.rb
+++ db/migrate/20210418230820_create_villagers.rb
@@ -1,4 +1,10 @@
 class CreateVillagers < ActiveRecord::Migration[5.2]
   def change
+    create_table :villagers do |t|
+      t.string :villagername
+      t.string :personality
+      t.string :species
+      t.integer :user_id
+    end
   end
 end
