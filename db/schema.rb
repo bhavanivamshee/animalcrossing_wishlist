fd866b85e1154fa5232277e86165cd946a8230ea
Bhavani <bhavanivamshee@gmail.com>
2021-04-18 22:37:07 -0500

    adds sessions, password to users table, salt and hashed password

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_230820) do
+ActiveRecord::Schema.define(version: 2021_04_19_033329) do
 
   create_table "users", force: :cascade do |t|
     t.string "islandname"
     t.string "username"
+    t.string "password_digest"
   end
 
   create_table "villagers", force: :cascade do |t|
