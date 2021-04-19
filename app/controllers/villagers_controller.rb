0c490da60370a84d910d98ee63fbf2f9ab86c3ae
Bhavani <bhavanivamshee@gmail.com>
2021-04-19 00:18:59 -0500

    project requirements met

--- app/controllers/villagers_controller.rb
+++ app/controllers/villagers_controller.rb
@@ -1,21 +1,33 @@
 class VillagersController < ApplicationController
 
     get "/villagers" do
+        if logged_in?
         @villagers = Villager.all
         erb :"villagers/index"
+        else
+            redirect "/login"
+        end
     end
 
     get "/villagers/new" do 
-        erb :"villagers/new"
+        if logged_in?
+            erb :"villagers/new"
+        else
+            redirect "/login"
+        end
     end
 
     get "/villagers/:id" do
-        @villager = Villager.find_by_id(params[:id])
-        erb :"villagers/show"
+        if logged_in?
+         @villager = Villager.find_by_id(params[:id])
+         erb :"villagers/show"
+        else
+            redirect "/login"
+        end
     end
 
     post "/villagers" do
-        villager = Villager.new(params)
+        villager = current_user.villager.build(params)
         if villager.save 
             redirect "villagers/#{villager.id}"
         else
@@ -25,8 +37,15 @@ class VillagersController < ApplicationController
     end
 
     get "/villagers/:id/edit" do
-        @villager = Villager.find_by_id(params[:id])
-        erb :"villagers/edit"
+        if logged_in?
+         @villager = Villager.find_by_id(params[:id])
+         if @villager.user.id == current_user.id
+            erb :"villagers/edit"
+         else
+            redirect "/login"
+        else
+            redirect "/login"
+        end
     end
 
     patch "/villagers/:id" do
